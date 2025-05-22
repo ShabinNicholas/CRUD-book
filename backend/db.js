7a99ac4d7f6c9eea0483155464fb0b01fe66547e
Shabin7 <shabin.nj@gmail.com>
2025-05-22 17:34:05 +0530

    22-05-2025

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,7 @@ mongoose.connect('mongodb://localhost:27017/BookBasicCRUD')
 const db = mongoose.connection
 db.on('error', (error) => {
     console.log("Connection error ", error);
-
 })
 db.once('open', () => {
     console.log("Database connected");
-
 })
--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,4 @@ app.use(express.json())
 app.use('/', routes)
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
-
 })
